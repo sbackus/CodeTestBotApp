65077497eb0ff228509fbc6a91873041d3bb172b
Stuart Childs <schilds@cyrusinnovation.com>
2014-03-21 16:10:45 -0400

    [Stuart and Sam] Simple form to create a new submission.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,3 +1,9 @@
-CodeTestBotApp.Router.map(function() {
+CodeTestBotApp.Router.reopen({
+    location: 'history'
+});
 
+CodeTestBotApp.Router.map(function() {
+    this.resource('submissions', function() {
+        this.route('new');
+    });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,5 @@ Rails.application.routes.draw do
   #   end
 
   root 'index#index'
+  get '*foo', :to => 'index#index'
 end
