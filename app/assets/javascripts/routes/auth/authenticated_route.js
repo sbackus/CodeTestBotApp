3acfe6ad0f0dc55244f8203ac031882e406d97c8
Stuart Childs <schilds@cyrusinnovation.com>
2014-03-26 14:23:53 -0400

    [Stuart and Sam] Add test for AuthenticatedRoute.

--- app/assets/javascripts/routes/auth/authenticated_route.js
+++ app/assets/javascripts/routes/auth/authenticated_route.js
@@ -2,7 +2,7 @@ CodeTestBotApp.AuthenticatedRoute = Ember.Route.extend({
     beforeModel: function(transition) {
         var authController = this.controllerFor('auth');
         if (!authController.get('loggedIn')) {
-            localStorage.setItem('previousTransition', transition.intent.url);
+            CodeTestBotApp.get('dataStore').setItem('previousTransition', transition.intent.url);
             return CodeTestBotApp.ApiSessionToken.acquire().then(function(response) {
                 authController.handleAuthResponse(response, false);
             });
