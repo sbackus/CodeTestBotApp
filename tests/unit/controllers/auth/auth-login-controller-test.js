cbcbf087d2039721327f850dd9cbfb06736d3312
Stuart Childs <schilds@cyrusinnovation.com>
2014-05-05 10:55:13 -0400

    testFor describe block wrapper.
    Allows for this.subject() in tests to return the object identified by the Ember fullname given to the testFor helper (e.g. controller:auth/login).

--- tests/unit/controllers/auth/auth-login-controller-test.js
+++ tests/unit/controllers/auth/auth-login-controller-test.js
@@ -1,17 +1,20 @@
 import { test , moduleFor } from 'ember-qunit';
 import WindowLocationHelper from 'code-test-bot-app/lib/window-location-helper';
+import testFor from '../../../helpers/test-for';
 
-moduleFor('controller:auth/login', 'Auth Login Controller');
+describe('Auth Login Controller', testFor('controller:auth/login', function() {
+    describe('login action', function() {
+        it('redirects to the auth_uri from the model', function() {
+            var uri = '/a/url';
+            var model = Ember.Object.create({ auth_uri: uri });
+            var controller = this.subject();
 
-test('login action: redirects to auth_uri from the model', function() {
-    var model = Ember.Object.create({ auth_uri: '/a/url' });
-    var controller = this.subject();
+            Ember.run(function() {
+                controller.set('model', model);
+                controller.send('login');
+            });
 
-    Ember.run(function() {
-        controller.set('model', model);
-        controller.send('login');
+            expect(WindowLocationHelper.setLocation.calledWith(uri)).to.be.true();
+        });
     });
-
-    ok(WindowLocationHelper.setLocation.calledOnce);
-    equal(WindowLocationHelper.setLocation.getCall(0).args[0], '/a/url');
-});
+}));
