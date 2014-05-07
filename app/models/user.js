fc45ed744cacda741ba201aa9f45935066965663
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-05-07 11:28:05 -0400

    Uncommented roles & say the user has no role if they don't have a role rather than make them an assessor.

--- app/models/user.js
+++ app/models/user.js
@@ -2,19 +2,22 @@ export default DS.Model.extend({
     name: DS.attr(),
     email: DS.attr(),
     editable: DS.attr(),
-    session: DS.hasMany('session')
-//    roles: DS.hasMany('role'),
-//
-//    roleNames: Ember.computed.mapBy('roles', 'name'),
-//
-//    roleDisplay: function() {
-//        var names = this.get('roleNames');
-//        if (names.contains('Administrator')) {
-//            return 'Administrator';
-//        } else if (names.contains('Recruiter')) {
-//            return 'Recruiter';
-//        } else {
-//            return 'Assessor';
-//        }
-//    }.property('roleNames')
+    session: DS.hasMany('session'),
+    roles: DS.hasMany('role'),
+
+    roleNames: Ember.computed.mapBy('roles', 'name'),
+
+    roleDisplay: function() {
+        var names = this.get('roleNames');
+        if (names.contains('Administrator')) {
+            return 'Administrator';
+        } else if (names.contains('Recruiter')) {
+            return 'Recruiter';
+        } else if (names.contains('Assessor')) {
+            return 'Assessor';
+        }
+        else {
+            return 'No current role';
+        }
+    }.property('roleNames')
 });
