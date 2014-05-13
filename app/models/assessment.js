484f3090020df71bf476e45263af0bf99920e104
Stuart Childs <childss@codecavern.org>
2014-05-13 17:27:40 -0400

    Show completed date for assessments.
    Closes #23.

--- app/models/assessment.js
+++ app/models/assessment.js
@@ -1,6 +1,11 @@
 export default DS.Model.extend({
     score: DS.attr(),
     notes: DS.attr(),
+    createdAt: DS.attr(),
     submission: DS.belongsTo('submission'),
-    assessor: DS.belongsTo('assessor')
+    assessor: DS.belongsTo('assessor'),
+
+    createdAtDisplay: function() {
+        return moment(this.get('createdAt')).format('l LT');
+    }.property('createdAt')
 });
