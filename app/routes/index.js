8a1c2c9eee680c5c5ef4f5fe77104b812c9b6d7f
Stuart Childs <childss@codecavern.org>
2014-05-13 22:30:52 -0400

    Hmm does taking out the redirect help?

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,1 @@
-import AuthenticatedRoute from 'code-test-bot-app/routes/authenticated-route';
-
-export default AuthenticatedRoute.extend({
-    beforeModel: function(transition) {
-        transition.abort();
-        this.transitionTo('/submissions');
-    }
-});
+export default Ember.Route.extend({});
