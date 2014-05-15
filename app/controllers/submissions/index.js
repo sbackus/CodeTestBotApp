799bafce0e6284abb6855ed89e65abc471466f14
Stuart Childs <childss@codecavern.org>
2014-05-15 11:19:31 -0400

    Hack in active/inactive submissions.
    (See #25)

--- app/controllers/submissions/index.js
+++ app/controllers/submissions/index.js
@@ -2,5 +2,8 @@ import UserAwareMixin from 'code-test-bot-app/mixins/user-aware';
 
 export default Ember.ArrayController.extend(UserAwareMixin, {
     sortProperties: ['createdAt'],
-    sortAscending: false
+    sortAscending: false,
+
+    activeSubmissions: Ember.computed.filterBy('content', 'active', true),
+    inactiveSubmissions: Ember.computed.filterBy('content', 'active', false)
 });
