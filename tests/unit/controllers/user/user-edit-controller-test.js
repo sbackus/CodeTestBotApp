cf8fddc08aaf922f55f3e1f73ea8571623cfcec8
Lisa van Gelder <lvangelder@cyrusinnovation.com>
2014-05-27 12:03:15 -0400

    Added unit test for user edit controller.

---
+++ tests/unit/controllers/user/user-edit-controller-test.js
@@ -0,0 +1,60 @@
+import testFor from '../../../helpers/test-for';
+import { randomElement } from '../../../helpers/utils';
+import { injectFakeStore } from '../../../helpers/data';
+
+describe('User Edit Controller', testFor('controller:user/edit', function() {
+    var controller;
+    var model, role, new_role, user, store;
+
+    before(function() {
+        controller = this.subject();
+        store = injectFakeStore(controller);
+
+        role = Ember.Object.create({
+            id: 1,
+            name: 'Administrator'
+        });
+
+        new_role = Ember.Object.create({
+            id: 2,
+            name: 'Assessor'
+        });
+
+        user = Ember.Object.create({
+            id: 1,
+            name: 'Bob',
+            email: 'bob@bob.com',
+            role: role
+        });
+
+        model = Ember.Object.create({
+            role: role
+        });
+
+        controller.set('model', model);
+        controller.set('selectedRole', new_role);
+        controller.set('id', 1);
+    });
+
+    describe('#changeRole', function() {
+
+        context('when role is selected', function() {
+            before(function() {
+                var find = sinon.stub(store, 'find');
+                find.withArgs('user').returns(user);
+                user.reopen({
+                    save: sinon.spy()
+                });
+            });
+
+            it('should have updated role for user', function() {
+                equal(user.get('role'), role);
+                Ember.run(function() {
+                    controller.send('changeRole');
+                });
+                equal(user.get('role'), new_role);
+                equal(user.save.callCount, 1);
+            });
+        });
+    });
+}));
