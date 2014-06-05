10ca1e40495d11f5d623f607541e6f295c0f5c02
Stuart Childs <stuart@childs.io>
2014-06-05 12:58:47 -0400

    Improve build script.
    Error out if there are unstaged/uncommitted changes.

--- build-dist.sh
+++ build-dist.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! `git diff --quiet`; then
+    echo "You have unstaged local changes. Commit or stash them before running the build."
+    exit
+fi
+
+if ! `git diff --cached --quiet`; then
+    echo "You have uncommitted staged changes. Commit or stash them before running the build."
+    exit
+fi
+
 set -e
 
 ember build --environment production
