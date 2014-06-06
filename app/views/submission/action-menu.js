00846bf7b8ce4922ee347ac9a8b4f6c060b443a8
Stuart Childs <stuart@childs.io>
2014-06-05 20:52:09 -0400

    Collapse view/delete buttons into split.

---
+++ app/views/submission/action-menu.js
@@ -0,0 +1,15 @@
+import Router from 'code-test-bot-app/router';
+
+export default Ember.View.extend({
+    templateName: 'submission/action-menu',
+
+    dropdownId: function() {
+        return 'drop-menu-' + this.elementId;
+    }.property(),
+
+    mainLink: function() {
+        // Can't just use link-to helper because it will preempt click handling from Foundation
+        // (i.e. it will just redirect to the route instead of dropping down the menu)
+        return Router.router.generate('submission.index', this.get('context'));
+    }.property('context')
+});
